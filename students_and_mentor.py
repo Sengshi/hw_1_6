7ed0a170ddab0c41f55b53c72e91edb8f4e6a809
aloster <asket2013@ya.ru>
2022-08-24 11:00:16 +1000

    Complete task 2

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -6,19 +6,34 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_lecture(self, lecturer, course_leads, grade_lecture):
+        if isinstance(lecturer, Lecturer) and course_leads in lecturer.courses_attached:
+            if course_leads in self.courses_in_progress:
+                if course_leads in lecturer.leads_the_course:
+                    lecturer.leads_the_course[course_leads] += [grade_lecture]
+                else:
+                    lecturer.leads_the_course[course_leads] = [grade_lecture]
+            else:
+                return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        
+        self.leads_the_course = {}
+
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
+        if isinstance(self, Reviewer) and isinstance(student, Student) and course in self.courses_attached:
+            if course in student.courses_in_progress:
+                if course in student.grades:
+                    student.grades[course] += [grade]
+                else:
+                    student.grades[course] = [grade]
             else:
-                student.grades[course] = [grade]
+                return 'Ошибка'
         else:
             return 'Ошибка'
 
@@ -30,15 +45,22 @@ class Reviewer(Mentor):
 class Lecturer(Mentor):
     pass
 
- 
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
+
+some_reviewer = Reviewer('Some', 'Buddy')
+some_reviewer.courses_attached += ['Python']
+
+some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 10)
+
+some_lecturer = Lecturer('Some', 'Buddy')
+some_lecturer.courses_attached += ['Python']
+best_student.rate_lecture(some_lecturer, 'Python', 10)
+best_student.rate_lecture(some_lecturer, 'Python', 10)
+# best_student.rate_lecture(some_lecturer, 'Git', 10) # тест наличия курса
+
 print(best_student.grades)
+print(some_lecturer.leads_the_course)
